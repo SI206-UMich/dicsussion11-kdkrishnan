9dc1c1bd64ee045253e44f85921d2b1dde584dbb
kdkrishnan <kirandk@umich.edu>
2022-11-16 13:55:14 -0500

    update

--- discussion11.py
+++ discussion11.py
@@ -38,14 +38,16 @@ def create_species_table(cur, conn):
 
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
+#we created a table with columns in SQLite 
 def create_patients_table(cur, conn):
-    pass
-
+    cur.execute("DROP TABLE IF EXISTS patients")
+    cur.execute("CREATE TABLE \"Patients\" (\"pet_id\" INTEGER PRIMARY KEY, \"name\" TEXT, \"species_id\" NUMBER, \"age\" INTEGER, \"cuteness\" INTEGER, \"aggressiveness\" NUMBER)")
+    conn.commit()
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
-    
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)", (0, "Fluffle", 0, 3, 90, 100))
+    conn.commit()
 
 # TASK 2
 # CODE TO ADD JSON TO THE TABLE
@@ -59,13 +61,31 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    ped_id = 1
+    for item in json_data:
+        name = item['name']
+        species = item['species']
+        age = item['age']
+        cuteness = item['cuteness']
+        aggressiveness = item['aggressiveness']
 
+        cur.execute('SELECT id FROM Species WHERE title = ?', (species,)) # (species,) is a tuple
+        species_id= int(cur.fetchone()[0])
 
+        print(ped_id,name,species_id,age,cuteness,aggressiveness)
+        cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)", (ped_id,name,species_id,age,cuteness,aggressiveness))
+
+        ped_id += 1
+    conn.commit()
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    cur.execute('SELECT name FROM Patients WHERE aggressiveness <= ?', (aggressiveness, ))
+    rows = cur.fetchall()
+    non = []
+    for row in rows:
+        non.append(row[0])
+    return non
 
 
 
